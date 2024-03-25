4e1e5df200778c2dd85410461f6ec658916e94f0
shreyzeous21 <shrey.sadhukhan21@gmail.com>
2024-03-25 23:28:58 +0530

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,56 @@
+import type { Metadata, Viewport } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { cn } from "@/lib/utils";
+import { SiteHeader } from "@/components/site-header";
+import { Providers } from "@/components/providers";
+import { siteConfig } from "@/config/site";
+import { SiteFooter } from "@/components/site-footer";
+import Head from "next/head";
+
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
+
+export const metadata: Metadata = {
+  title: siteConfig.name,
+  description: siteConfig.description,
+};
+
+export const viewport: Viewport = {
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "white" },
+    { media: "(prefers-color-scheme: dark)", color: "black" },
+  ],
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="scroll-pt-[3.5rem]">
+      <head>
+        <link
+          rel=" icon"
+          href={siteConfig.titleIcon}
+          type="image/svg+xml"
+          sizes="any"
+        />
+      </head>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          inter.variable
+        )}
+      >
+        <Providers>
+          <div className="relative flex min-h-dvh flex-col bg-background">
+            <SiteHeader />
+            <main className="flex-1">{children}</main>
+            <SiteFooter />
+          </div>
+        </Providers>
+      </body>
+    </html>
+  );
+}
---
+++ config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+  name: "Shrey | xDEV",
+  url: "https://shrey-contact-me.vercel.app/",
+  description: "Nextjs 14 blog using velite, tailwind and shadcn",
+  author: "Shrey Sadhukhan",
+  links: {
+    github: "https://github.com/shreyzeous21",
+    personalSite: "https://shrey-contact-me.vercel.app/",
+  },
+  titleIcon:"/app/icons.svg"
+};
+
+export type SiteConfig = typeof siteConfig;
