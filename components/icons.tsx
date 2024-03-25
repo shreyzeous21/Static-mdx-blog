fb6f8ed8bf4a9aa8495d66eb1bc9a4afafb4f647
shreyzeous21 <shrey.sadhukhan21@gmail.com>
2024-03-25 23:36:30 +0530

    change

--- components/icons.tsx
+++ components/icons.tsx
@@ -4,7 +4,7 @@ export const Icons = {
   logo: (props: IconProps) => (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      class="icon icon-tabler icon-tabler-bat"
+      className="icon icon-tabler icon-tabler-bat"
       width="44"
       height="44"
       viewBox="0 0 24 24"
